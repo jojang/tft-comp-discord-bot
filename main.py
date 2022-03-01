9a136e185f2a77796125c4dde5db6f932ddfe657
joesjang <36047664+jojang@users.noreply.github.com>
2022-03-01 04:01:04 +0000

    added champion feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import tftdecks
+from PIL import Image
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,16 +21,41 @@ async def on_message(message):
   if message.author == client.user: #prevents infinite loops with the bot
     return
 
-  if message.content.startswith(f'!hello'): #introduction
-    await message.channel.send('Hello! I am the TFT Deck Bot. Please use the !allcomps command to view the list of the most used team compositions at the moment. Enter a team composition from the list to receive a link to its guide.')
+  if message.content.startswith(f'!commands'): #introduction
+    await message.channel.send('Hello! I am the TFT Deck Bot. Please use the !allcomps or !champs command to begin.')
 
   showComps = tftdecks_inst.search()
+  cleanComps = "Comps: \n"
+  
+  for i in showComps:
+    cleanComps = cleanComps + i + ', '
 
+  cleanComps = cleanComps[:-2]
+  
   if f'!allcomps' in message.content: #!allcomps command
-    await message.channel.send(showComps)
+    await message.channel.send(cleanComps)
 
-  if str(message.content) in showComps: #enter the name of the team comp from !allcomps command
+  if message.content in showComps: #enter the name of the team comp from !allcomps command
     findPos = showComps.index(message.content)  #finds the index of the team composition of the user's input
-    await message.channel.send(tftdecks_inst.findLink(findPos)) 
+    await message.channel.send(tftdecks_inst.findLink(findPos))
+
+  showChamps = tftdecks_inst.champs
+  cleanChamps = "Champions: \n"
+  
+  for i in showChamps:
+    cleanChamps = cleanChamps + i + ', '
+
+  cleanChamps = cleanChamps[:-2]
+  
+  if message.content.startswith(f'!champs'):
+    await message.channel.send(cleanChamps)
+  
+  if message.content.startswith(f'!') and message.content[1:].lower() in showChamps: #enter the name of the champion from !champs command
+    champ = message.content.lower()
+    items = tftdecks_inst.findItems(champ)
+    
+    await message.channel.send(items[0])
+    await message.channel.send(items[1])
+    await message.channel.send(items[2])
   
 client.run(TOKEN)
--- tftdecks.py
+++ tftdecks.py
@@ -8,7 +8,10 @@ class TftDecks:
 
     self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'}
     self.url = 'https://lolchess.gg/meta'
+    self.champs = ['ahri', 'alistar', 'ashe', 'blitzcrank', 'brand', 'braum', 'caitlyn', 'camille', 'chogath', 'corki', 'darius', 'draven', 'ekko', 'ezreal', 'galio', 'gangplank', 'gnar', 'illaoi', 'irelia', 'jarvaniv', 'jayce', 'jhin', 'jinx', 'kaisa', 'kassadin', 'khazix', 'leona', 'lucian', 'lulu', 'malzahar', 'missfortune', 'morgana', 'nocturne', 'orianna', 'poppy', 'quinn', 'reksai', 'renata', 'sejuani', 'senna', 'seraphine', 'silco', 'singed', 'sivir', 'swain', 'syndra', 'tahmkench', 'talon', 'tryndamere', 'twitch', 'veigar', 'vex', 'vi', 'viktor', 'warwick', 'zac', 'zeri', 'ziggs', 'zilean', 'zyra']
+    self.champurl = 'https://lolchess.gg/champions/set6.5/'
 
+  
   def search(self):
     """returns a list of strings of all the team compositions listed on the Lolchess.gg website."""
 
@@ -28,6 +31,7 @@ class TftDecks:
 
     return finalList
 
+  
   def findLink(self, index):
     """takes an integer 'index' as input, uses Beautiful Soup to scrape all of the necessary links on the page, and returns the desired comp from a list of the links as strings using the inputted index."""
 
@@ -45,3 +49,21 @@ class TftDecks:
       
     return allLinks[index]
 
+  
+  def findItems(self, champ):
+      
+    page = requests.get(self.champurl + champ, headers =     self.headers)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+  
+    itemsTab = soup.find('div', class_= 'guide-champion-detail__recommend-items__content')
+
+    items = itemsTab.find_all('img')
+
+    allItems = []
+    for i in items:
+      imageLink = i['src']
+      imageLink = 'https:' + imageLink
+      allItems.append(imageLink)
+    
+    return allItems
